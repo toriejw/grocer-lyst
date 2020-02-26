da4f48852ebbf854600e1574431c3b2c64e11897
Torie Joy-Warren <torie@carezone.com>
2020-02-25 22:04:43 -0500

    Create ingredients table

---
+++ db/migrate/20200226022602_create_ingredients.rb
@@ -0,0 +1,8 @@
+class CreateIngredients < ActiveRecord::Migration[6.0]
+  def change
+    create_table :ingredients do |t|
+      t.text :name, null: false
+      t.timestamps null: false
+    end
+  end
+end
