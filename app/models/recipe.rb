b1ef795e98562c3fc6ff559f889e4b688dbd8f35
Torie Joy-Warren <torie@carezone.com>
2020-04-30 20:47:25 -0400

    Alphabetize recipes

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,5 +3,7 @@ class Recipe < ApplicationRecord
   has_many :ingredients, inverse_of: :recipe
   accepts_nested_attributes_for :ingredients, reject_if: :all_blank, allow_destroy: true
 
+  scope :alphabetized, -> { sort_by { |recipe| recipe.name} }
+
   validates :name, presence: true
 end
