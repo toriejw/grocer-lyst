0884b40228d14b60a571ba651869eebf8fae765a
Torie Joy-Warren <torie@carezone.com>
2020-05-02 14:54:57 -0400

    Seed teaspoon & tablespoon as measurement units

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,9 @@
 # Measurement units
-measurement_unit_types = %w(cup)
+measurement_unit_types = %w(
+  cup
+  teaspoon
+  tablespoon
+)
 
 measurement_unit_types.each do |unit_type|
   MeasurementUnit.find_or_create_by(name: unit_type)
